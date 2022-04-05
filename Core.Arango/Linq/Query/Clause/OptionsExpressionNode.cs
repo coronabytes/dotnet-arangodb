e3d224b1b3ba7372d748261b45613c4ffab18fea
Erffun <Erffun@gmail.com>
2022-04-05 15:16:39 +0430

    Feature/add options query extention (#50)
    * OptionsExpressionNodes class added
    
    * VisitOptionsClause method added to ArangoModelVisitor
    
    * OptionsClause added
    
    * OptionsExpressionNode registered to AqlParser
    
    * Options method added to ArangoQueryableExtensions
    
    * Option test added
    
    * readme updated

---
+++ Core.Arango/Linq/Query/Clause/OptionsExpressionNode.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Core.Arango.Relinq;
+using Core.Arango.Relinq.Parsing.Structure.IntermediateModel;
+
+namespace Core.Arango.Linq.Query.Clause
+{
+    internal class OptionsExpressionNode : MethodCallExpressionNodeBase
+    {
+        public static readonly MethodInfo[] SupportedMethods =
+        {
+            LinqUtility.GetSupportedMethod(() => ArangoQueryableExtensions.Options<object>(null, () => null))
+        };
+
+        public OptionsExpressionNode(MethodCallExpressionParseInfo parseInfo, Expression options)
+            : base(parseInfo)
+        {
+            Options = options;
+        }
+
+        public Expression Options { get; }
+
+
+        public override Expression Resolve(ParameterExpression inputParameter, Expression expressionToBeResolved,
+            ClauseGenerationContext clauseGenerationContext)
+        {
+            LinqUtility.CheckNotNull("inputParameter", inputParameter);
+            LinqUtility.CheckNotNull("expressionToBeResolved", expressionToBeResolved);
+
+            return Source.Resolve(inputParameter, expressionToBeResolved, clauseGenerationContext);
+        }
+
+        protected override void ApplyNodeSpecificSemantics(QueryModel queryModel,
+            ClauseGenerationContext clauseGenerationContext)
+        {
+            LinqUtility.CheckNotNull("queryModel", queryModel);
+
+            queryModel.BodyClauses.Add(new OptionsClause(
+                Options
+            ));
+        }
+    }
+}
