694cbb066b5c3ce9f6542a7be8580ac1ba20fd71
jung <jung@corona-bytes.net>
2022-06-21 00:30:23 +0200

    more aql array functions

--- Core.Arango/Linq/Aql.Array.cs
+++ Core.Arango/Linq/Aql.Array.cs
@@ -106,5 +106,87 @@ namespace Core.Arango.Linq
         {
             throw E;
         }
+
+        /// <summary>
+        ///     Append value to anyArray (right side).
+        /// </summary>
+        [AqlFunction("PUSH")]
+        public static T[] Push<T>(IEnumerable<T> array, T value)
+        {
+            throw E;
+        }
+
+        /// <summary>
+        ///     Append value to anyArray (right side).
+        /// </summary>
+        [AqlFunction("PUSH")]
+        public static T[] Push<T>(IEnumerable<T> array, T value, bool unique)
+        {
+            throw E;
+        }
+
+        /// <summary>
+        ///     Remove the last element of array.
+        /// </summary>
+        [AqlFunction("POP")]
+        public static T[] Pop<T>(IEnumerable<T> array)
+        {
+            throw E;
+        }
+
+        /// <summary>
+        ///     Remove the first element of anyArray.
+        /// </summary>
+        [AqlFunction("SHIFT")]
+        public static T[] Shift<T>(IEnumerable<T> array)
+        {
+            throw E;
+        }
+
+        /// <summary>
+        ///     Prepend value to anyArray (left side).
+        /// </summary>
+        [AqlFunction("UNSHIFT")]
+        public static T[] Unshift<T>(IEnumerable<T> array, T value)
+        {
+            throw E;
+        }
+
+        /// <summary>
+        ///     Prepend value to anyArray (left side).
+        /// </summary>
+        [AqlFunction("UNSHIFT")]
+        public static T[] Unshift<T>(IEnumerable<T> array, T value, bool unique)
+        {
+            throw E;
+        }
+
+        /// <summary>
+        ///     Return all unique elements in anyArray.
+        ///     To determine uniqueness, the function will use the comparison order.
+        /// </summary>
+        [AqlFunction("UNIQUE")]
+        public static T[] Unique<T>(IEnumerable<T> array)
+        {
+            throw E;
+        }
+
+        /// <summary>
+        ///     Return an array with its elements reversed.
+        /// </summary>
+        [AqlFunction("REVERSE")]
+        public static T[] Reverse<T>(IEnumerable<T> array)
+        {
+            throw E;
+        }
+
+        /// <summary>
+        ///     Calculate the Jaccard index of two arrays.
+        /// </summary>
+        [AqlFunction("JACCARD")]
+        public static double Jaccard(object array1, object array2)
+        {
+            throw E;
+        }
     }
 }
